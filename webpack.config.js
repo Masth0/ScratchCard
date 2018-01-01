e1e64ccf42dffbe0a4ec3fcb7bcdd3dd0341b72c
unknown <dev.mastho@gmail.com>
2018-01-01 01:02:31 +0100

    update config etc

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,7 @@
 const webpack = require('webpack');
 const path = require('path');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
-const Bs = require('browser-sync');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'dev';
 
 /*---- Javascript config -----------------------------------------------------------------------*/
@@ -15,12 +12,11 @@ const JS = {
   devtool: isDev ? 'inline-source-map' : false,
   entry: {
     scratchcard: './ScratchCard.ts',
-    '../../../build/scratchcard': './ScratchCard.ts'
   },
   output: {
     filename: '[name].js',
     chunkFilename: '[name].bundle.js',
-    path: __dirname + '/docs/js/lib',
+    path: __dirname + '/build',
     library: undefined,
     libraryTarget: 'umd'
   },
@@ -36,56 +32,7 @@ const JS = {
       }
     ]
   },
-  plugins: [
-    new BrowserSyncPlugin({
-      // browse to http://localhost:3000/ during development, 
-      // ./public directory is being served 
-      host: 'localhost',
-      port: 3000,
-      server: { baseDir: ['docs'] },
-      files: ['./src/js/**/*.ts', './src/scss/**/*.scss']
-    })
-  ]
-};
-
-/*---- SCSS / CSS config -----------------------------------------------------------------------*/
-const SCSS = {
-  context: path.resolve('./src/scss/'),
-  name: 'scss',
-  watch: isDev,
-  entry: {
-    styles: ['./style.scss']
-  },
-  output: {
-    filename: 'style.css',
-    path: __dirname + '/docs/css'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: [
-            {loader: 'css-loader', options: {importLoaders: 1, minimize: !isDev}},
-            {loader: 'resolve-url-loader'},
-            {loader: 'postcss-loader', options: {
-              plugins: (loader) => [
-                require('autoprefixer')()
-              ]
-            }},
-            {loader: 'sass-loader'}
-          ]
-        }),
-      }
-    ]
-  },
-  plugins: [
-    new ExtractTextPlugin({
-      filename: 'style.css',
-      allChunks: true,
-    })
-  ]
+  plugins: []
 };
 
 /*---- For production -----------------------------------------------------------------------*/
@@ -94,15 +41,10 @@ if (!isDev) {
   JS.plugins.push(new CleanWebpackPlugin(['./build'], { // remove ./build/js
     root: path.resolve('./'),
     verbose: true,
-    dry: true,
+    dry: false,
   }));
   JS.plugins.push(new UglifyJsPlugin());
-  SCSS.plugins.push(new CleanWebpackPlugin(['docs/css'], { // remove ./dist/css
-    root: path.resolve('./'),
-    verbose: true,
-    dry: true,
-  }));
 }
 
 /*---- Export -----------------------------------------------------------------------*/
-module.exports = [JS, SCSS];
+module.exports = [JS];
